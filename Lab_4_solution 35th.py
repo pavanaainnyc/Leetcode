d7342223082b9be27798567073c91d031d9bd5b9
pavanaainnyc <pavanaasnairbsc24@rvu.edu.in>
2026-02-06 10:47:36 +0530

    Lab_4_solution 35th to Lab_4_solution 35.py

---
+++ Lab_4_solution 35th.py
@@ -0,0 +1,14 @@
+def searchInsert(nums, target):
+  low, high = 0, len(nums) 1
+  while low <= high:
+    mid (low + high) // 2
+    if nums[mid] == target:
+      return mid
+    elif nums[mid] < target:
+      low mid + 1
+    else:
+      high mid 1
+  return low
+print(searchInsert ([1,3,5,6, 5)) #2
+print(searchInsert([1,3,5,6], 2)) # 1
+print(searchInsert ([1,3,5,6], 7)) #4
