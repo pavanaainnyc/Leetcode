157e4a82ee2085f19c784cb924cf6306f15b5b9c
pavanaainnyc <pavanaasnairbsc24@rvu.edu.in>
2026-03-25 17:11:33 +0530

    Delete Perfect array.py

--- Perfect array.py
+++
@@ -1,7 +0,0 @@
-arr = list(map(int, input().split()))
-flag = True
-for i in range(len(arr)):
-    if arr[i] != i:
-        flag = False
-        break
-print("Perfect Array" if flag else "Not Perfect")
